444ae5be6c13b25d34697e1334a3bb4b942d51d2
Sanjai229 <sanjaicena123@gmail.com>
2025-12-10 14:55:04 +1030

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+model = joblib.load("model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+@app.get("/")
+def home():
+    return {"message": "Churn Prediction API Running"}
+
+@app.post("/predict")
+def predict(data: dict):
+    values = np.array(list(data.values())).reshape(1, -1)
+    scaled = scaler.transform(values)
+    pred = model.predict(scaled)[0]
+    prob = model.predict_proba(scaled)[0][1]
+    return {
+        "prediction": int(pred),
+        "probability": float(prob)
+    }
+
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+model = joblib.load("model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+@app.get("/")
+def home():
+    return {"message": "Churn Prediction API Running"}
+
+@app.post("/predict")
+def predict(data: dict):
+    values = np.array(list(data.values())).reshape(1, -1)
+    scaled = scaler.transform(values)
+    pred = model.predict(scaled)[0]
+    prob = model.predict_proba(scaled)[0][1]
+    return {
+        "prediction": int(pred),
+        "probability": float(prob)
+    }
